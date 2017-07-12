d78a27f3bda85aab95ce8e893c818ff87bca0bc4
Nicole Cayouette <nmcayouette@fullsail.edu>
2017-07-12 13:32:54 -0400

    Produce 3 MVC

--- myframework/views/header.php
+++ myframework/views/header.php
@@ -1,23 +1,23 @@
-<!--<!DOCTYPE html>-->
-<!--<html>-->
-<!--<head>-->
-<!--    <meta charset="utf-8">-->
-<!--    <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
-<!--    <meta name="viewport" content="width=device-width, initial-scale=1">-->
-<!--    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->-->
-<!--    <meta name="description" content="">-->
-<!--    <meta name="author" content="">-->
-<!--    <link rel="icon" href="../../favicon.ico">-->
-<!---->
-<!--    <title>Navbar Template for Bootstrap</title>-->
-<!---->
-<!--    <!-- Bootstrap core CSS -->-->
-<!--    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">-->
-<!---->
-<!--    <!-- Custom styles for this template -->-->
-<!--    <link href="../assets/css/custom.css" rel="stylesheet">-->
-<!---->
-<!--</head>-->
-<!---->
-<!---->
-<!--<body>-->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Navbar Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/custom.css" rel="stylesheet">
+
+</head>
+
+
+<body>
